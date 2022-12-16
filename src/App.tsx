8fb768e073d75d5a7da5706555b203ca90c6feda
Prohor <prohor-yakovlev@mail.ru>
2022-12-16 16:51:06 +0300

    UniversalButton

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,36 @@
-import React, {MouseEvent} from 'react';
+import React, {MouseEvent, useState} from 'react';
 import './App.css';
-import Button from "./component/Button";
 
 
 function App() {
-    const Button1Foo = (subscriber: string, age: number) => {
-        console.log(subscriber, age)
-    }
+    const [money, setMoney] = useState([
+        {banknots: 'Dollars', value: 100, number: ' a1234567890'},
+        {banknots: 'Dollars', value: 50, number: ' z1234567890'},
+        {banknots: 'RUBLS', value: 100, number: ' w1234567890'},
+        {banknots: 'Dollars', value: 100, number: ' e1234567890'},
+        {banknots: 'Dollars', value: 50, number: ' c1234567890'},
+        {banknots: 'RUBLS', value: 100, number: ' r1234567890'},
+        {banknots: 'Dollars', value: 50, number: ' x1234567890'},
+        {banknots: 'RUBLS', value: 50, number: ' v1234567890'},
+    ])
 
-    const Button2Foo = (subscriber: string) => {
-        console.log(subscriber)
+    let currentMoney = money.filter((filteredMoney)=>filteredMoney.banknots ==='RUBLS')
 
-    }
-
-    const Button3Foo = () => {
-        console.log("im stupid button")
-    }
     return (
-        <div className="App">
-            <Button name={"MyYouTobeChanel-1"} callBack={() => Button1Foo('Im Vasya', 21)}/>
-            <Button name={"MyYouTobeChanel-2"} callBack={() => Button2Foo('Im Prohor')}/>
-            {/*<Button name={"Delete"}/>*/}
-            <Button name={"MyYouTobeChanel-3"} callBack={Button3Foo}/>
-        </div>
+        <ul>
+            {money.map((objFromMomeyArr,index) => {
+                return (
+                        <li key={index}>
+                            <span>{objFromMomeyArr.banknots}</span>
+                            <span>{objFromMomeyArr.value}</span>
+                            <span>{objFromMomeyArr.number}</span>
+                        </li>
+                )
+            })}
+        </ul>
+
     );
+
 }
 
 export default App;
