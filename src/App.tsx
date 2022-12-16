587829d17ad37c1571cc8ef1a09ebd9b63d3a3b8
Prohor <prohor-yakovlev@mail.ru>
2022-12-16 11:10:07 +0300

    UniversalButton

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,8 @@ import './App.css';
 import Button from "./component/Button";
 
 
-
-
 function App() {
-    const Button1Foo = (subscriber : string, age: number) => {
+    const Button1Foo = (subscriber: string, age: number) => {
         console.log(subscriber, age)
     }
 
@@ -14,11 +12,16 @@ function App() {
         console.log(subscriber)
 
     }
+
+    const Button3Foo = () => {
+        console.log("im stupid button")
+    }
     return (
         <div className="App">
-            <Button name={"MyYouTobeChanel-1"} callBack={()=>Button1Foo('Im Vasya', 21)}/>
-            <Button name={"MyYouTobeChanel-2"} callBack={()=>Button2Foo('Im Prohor')}/>
+            <Button name={"MyYouTobeChanel-1"} callBack={() => Button1Foo('Im Vasya', 21)}/>
+            <Button name={"MyYouTobeChanel-2"} callBack={() => Button2Foo('Im Prohor')}/>
             {/*<Button name={"Delete"}/>*/}
+            <Button name={"MyYouTobeChanel-3"} callBack={Button3Foo}/>
         </div>
     );
 }
